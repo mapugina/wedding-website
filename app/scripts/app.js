454580e4cca44a0526c97d9d923578429d652ab7
Michael Pugina <michael.pugina@hughes.com>
2016-02-28 10:50:51 -0700

    init

---
+++ app/scripts/app.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name untitledApp
+ * @description
+ * # untitledApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('untitledApp', [
+    'ngAnimate',
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/purposal', {
+        templateUrl: 'views/purposal.html'
+      })
+      .when('/date', {
+        templateUrl: 'views/date.html'
+      })
+      .when('/registries', {
+        templateUrl: 'views/registries.html'
+      })
+      .when('/gallery', {
+        templateUrl: 'views/gallery.html'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
